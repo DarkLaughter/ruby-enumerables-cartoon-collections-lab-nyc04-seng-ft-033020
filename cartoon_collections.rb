447a69187b9471392b315d35cdb82c54ef6f8420
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-22 18:42:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.select {|word| (word.length) > 4}
+  array.select {|word| return (word.length) > 4}
 end
 
 def find_the_cheese# code an argument here
